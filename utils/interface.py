4f2fbc21b1d65bc2b3d7e8f065fbdb1aecf90cd3
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-17 16:46:33 +1000

    Create Button class

---
+++ utils/interface.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from pygame import Surface
+from pygame.font import SysFont
+from utils.base import Sprite
+
+if TYPE_CHECKING:
+    from pygame.sprite import _Group
+
+
+class Button(Sprite):
+    def __init__(
+        self,
+        image: Surface | str,
+        on_press: function,
+        pos: tuple[int, int] = None,
+        *groups: _Group,
+    ) -> None:
+        if isinstance(image, str):
+            image = SysFont("Arial", 20).render(image, True, (0, 0, 0))
+        super().__init__(image, pos=pos, on_left_click=on_press, *groups)
