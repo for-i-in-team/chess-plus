f1667f35910a8f68851d946031abc0091869ffb2
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-17 16:45:52 +1000

    Create Scene class

---
+++ utils/scene.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+from abc import ABC
+
+if TYPE_CHECKING:
+    from utils.base import GameObject
+
+
+@ABC
+class Scene:
+    def __init__(self) -> None:
+        self.objects: list[GameObject] = []
+
+    def update(self) -> None:
+        for obj in self.objects:
+            obj.update()
+
+    def draw(self, screen) -> None:
+        for obj in self.objects:
+            obj.draw(screen)
+
+    def handle_event(self, event) -> None:
+        for obj in self.objects:
+            obj.handle_event(event)
