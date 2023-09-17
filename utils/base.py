8c053f2d269a20b440432784bf6656edcedb21b6
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-17 16:44:10 +1000

    Create GameObject & Sprite Classes

---
+++ utils/base.py
@@ -0,0 +1,183 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from abc import ABC
+
+from pygame.sprite import Sprite as BaseSprite
+from pygame import transform, MOUSEBUTTONDOWN, MOUSEBUTTONUP
+
+if TYPE_CHECKING:
+    from pygame import Surface, Rect
+    from pygame.sprite import _Group
+    from pygame.event import Event
+
+LEFTMOUSE = 1
+RIGHTMOUSE = 3
+
+
+@ABC
+class GameObject(BaseSprite):
+    def handle_event(self, event: Event) -> None:
+        "Base function for handling events, will simply pass unless overridden by a child class"
+        pass
+
+    def update(self) -> None:
+        "Base function for updating the object on each frame, will simply pass unless overridden by a child class"
+        pass
+
+    def draw(self, surface: Surface) -> None:
+        "Base function for drawing the object on each frame, will simply pass unless overridden by a child class"
+        pass
+
+
+@ABC
+class Sprite(GameObject):
+    def __init__(
+        self,
+        image: Surface,
+        pos: tuple[int, int] | None = None,
+        height: int = None,
+        width: int = None,
+        rotation: int = 0,
+        transparency: float = 1,
+        flip: bool = False,
+        on_left_click: callable[Event] = None,
+        on_right_click: callable[Event] = None,
+        *groups: _Group,
+    ) -> None:
+        super().__init__(*groups)
+        self._pos: tuple[int, int] = pos or (0, 0)
+        self._image: Surface = image
+        self._height: int | None = height
+        self._width: int | None = width
+        self._rotation: int = rotation
+        self._transparency: float = transparency
+        self._flip: bool = flip
+        self._image_needs_transform: bool = True
+        self._transformed_image: Surface = None
+        self._rect_needs_update: bool = True
+        self._rect: Rect = None
+
+        self._left_mouse_down: bool = False
+        self._right_mouse_down: bool = False
+        self.on_left_click: function = on_left_click
+        self.on_right_click: function = on_right_click
+
+    @property
+    def pos(self) -> tuple[int, int]:
+        return self._pos
+
+    @pos.setter
+    def pos(self, value: tuple[int, int]):
+        if value != self._pos:
+            self._pos = value
+            self._rect_needs_update = True
+
+    @property
+    def height(self) -> int | None:
+        return self._height
+
+    @height.setter
+    def height(self, value: int):
+        if value != self._height:
+            self._height = value
+            self._image_needs_transform = True
+
+    @property
+    def width(self) -> int | None:
+        return self._width
+
+    @width.setter
+    def width(self, value: int):
+        if value != self._width:
+            self._width = value
+            self._image_needs_transform = True
+
+    @property
+    def rotation(self) -> int:
+        return self._rotation
+
+    @rotation.setter
+    def rotation(self, value: int):
+        if value != self._rotation:
+            self._rotation = value
+            self._image_needs_transform = True
+
+    @property
+    def transparency(self) -> float:
+        return self._transparency
+
+    @transparency.setter
+    def transparency(self, value: float):
+        if value != self._transparency:
+            self._transparency = value
+            self._image_needs_transform = True
+
+    @property
+    def flip(self) -> bool:
+        return self._flip
+
+    @flip.setter
+    def flip(self, value: bool):
+        if value != self._flip:
+            self._flip = value
+            self._image_needs_transform = True
+
+    def transform_image(self) -> None:
+        new_image = self._image.copy()
+        if self._transparency != 1:
+            new_image.set_alpha(self._transparency)
+        new_image = self.scale_image(new_image)
+        if self._rotation != 0:
+            new_image = transform.rotate(new_image, self._rotation)
+        if self._flip:
+            new_image = transform.flip(new_image, True, False)
+        self._transformed_image = new_image
+        self.update_rect()
+
+    def scale_image(self, new_image: Surface) -> Surface:
+        width = self._width
+        height = self._height
+        if width is None and height is None:
+            return new_image
+
+        if width is None:
+            width = new_image.get_width() * self._height / new_image.get_height()
+        elif height is None:
+            height = new_image.get_height() * self._width / new_image.get_width()
+
+        if width != new_image.get_width() or height != new_image.get_height():
+            return transform.scale(new_image, (width, height))
+
+    def update_rect(self) -> None:
+        self._rect = self._transformed_image.get_rect(
+            centerx=int(self.pos[0]), centery=int(self.pos[1])
+        )
+
+    def touches(self, pos: tuple[int, int]) -> bool:
+        if self._rect == None:
+            self.update_rect()
+        return self._rect.collidepoint(pos)
+
+    def check_clicks(self, event: Event) -> None:
+        if event.type == MOUSEBUTTONDOWN and self.touches(event.pos):
+            self._left_mouse_down = event.button == LEFTMOUSE
+            self._right_mouse_down = event.button == RIGHTMOUSE
+        elif event.type == MOUSEBUTTONUP:
+            if event.button == LEFTMOUSE and self._left_mouse_down:
+                self._left_mouse_down = False
+                if self.on_left_click is not None:
+                    self.on_left_click()
+            elif event.button == RIGHTMOUSE and self._right_mouse_down:
+                self._right_mouse_down = False
+                if self.on_right_click is not None:
+                    self.on_right_click()
+
+    def draw(self, surface: Surface) -> None:
+        if self._image_needs_transform:
+            self.transform_image()
+            self._image_needs_transform = False
+
+        surface.blit(self._transformed_image, self._rect)
+
+    def handle_event(self, event: Event) -> None:
+        self.check_clicks(event)
