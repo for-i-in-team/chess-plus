bc38a688205b3bd01e9f6e2d40d06ea47e5f83f0
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-19 17:23:25 +1000

    Add ChessBoardView to PlayChess Scene

--- scenes/play_chess.py
+++ scenes/play_chess.py
@@ -1,6 +1,21 @@
+import pygame
+
+from models.board import ChessBoard
+from view.board import ChessBoardView
+from utils.base import Coordinate
 from utils.scene import Scene
 
 
 class PlayChess(Scene):
     def __init__(self) -> None:
         super().__init__()
+        board = ChessBoard(8)
+        self.objects.append(
+            ChessBoardView(
+                board,
+                Coordinate(
+                    pygame.display.get_window_size()[0] / 2,
+                    pygame.display.get_window_size()[1] / 2,
+                ),
+            )
+        )
