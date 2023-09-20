62356c9a588fec3bce255f8c33be3fcc82b4619a
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-20 17:26:27 +1000

    Refactor Event System

--- events.py
+++ events.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod, abstractproperty
+
 from pygame.event import Event, post
 from pygame import USEREVENT
 
@@ -6,6 +8,28 @@ from utils.scene import Scene
 SCENECHANGEEVENT = USEREVENT + 1
 
 
-def scene_change(scene: Scene) -> Event:
-    event = Event(SCENECHANGEEVENT, scene=scene)
-    post(event)
+class BaseEvent(ABC):
+    @abstractproperty
+    def event_type(self):
+        pass
+
+    @abstractmethod
+    def _get_kwargs(self):
+        pass
+
+    def fire(self):
+        event = Event(self.event_type, self._get_kwargs())
+        post(event)
+
+
+class SceneChangeEvent(BaseEvent):
+    def __init__(self, scene) -> None:
+        self.scene = scene
+        super().__init__()
+
+    @property
+    def event_type(self):
+        return SCENECHANGEEVENT
+
+    def _get_kwargs(self):
+        return {"scene": self.scene}
