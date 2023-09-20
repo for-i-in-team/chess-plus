dbfdf527fd40bd2db7ef9cce242fdc59303334e6
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-21 09:18:45 +1000

    Update view to spawn selection display on click

--- view/board.py
+++ view/board.py
@@ -2,10 +2,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pygame
+from pygame.event import Event
 
+from event_types import CHESSSQUARECLICKEVENT
+from game_events import ChessSquareClickEvent
 from utils.base import ParentObject, Sprite, Coordinate
 from view.piece import PieceView
 from models.base_piece import ChessPiece
+from view.square_selection import SquareSelection
 
 if TYPE_CHECKING:
     from models.board import ChessBoard, ChessSquare
@@ -20,6 +24,7 @@ class ChessBoardView(ParentObject):
         *groups,
     ) -> None:
         super().__init__(groups)
+        self.square_selection: SquareSelection | None = None
         self.board: ChessBoard = board
         self.pos: Coordinate = pos
         if size is None:
@@ -29,7 +34,7 @@ class ChessBoardView(ParentObject):
         self.size = size
 
         square_size = int(self.size.x / max(board.size.x, board.size.y))
-        top_left = self.pos - int(self.size.x / 2)
+        top_left = self.top_left
         for row in self.board.squares:
             for square in row:
                 self.children.append(
@@ -39,6 +44,24 @@ class ChessBoardView(ParentObject):
                         (top_left + square.coords * square_size) + int(square_size / 2),
                     )
                 )
+
+    @property
+    def top_left(self):
+        return Coordinate(
+            self.pos.x - int(self.size.x / 2), self.pos.y - int(self.size.y / 2)
+        )
+
+    def spawn_selection_display(self, square: ChessSquareView):
+        if self.square_selection is not None:
+            self.children.remove(self.square_selection)
+            self.square_selection = None
+        self.square_selection = SquareSelection(self, square)
+        self.children.append(self.square_selection)
+
+    def handle_event(self, event: Event) -> None:
+        if event.type == CHESSSQUARECLICKEVENT:
+            self.spawn_selection_display(event.square)
+        return super().handle_event(event)
 
 
 class ChessSquareView(Sprite):
@@ -50,7 +73,9 @@ class ChessSquareView(Sprite):
             if square.piece is not None
             else None
         )
-        super().__init__(self.get_image(), pos)
+        super().__init__(
+            self.get_image(), pos, on_left_click=ChessSquareClickEvent(self).fire
+        )
 
     def set_piece(self, piece: ChessPiece):
         if self.piece_view is None or piece != self.piece_view.piece:
