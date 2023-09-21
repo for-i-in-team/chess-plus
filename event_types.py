a4497a3b6fbca68a9b01e01d9a2b3f86322230e7
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-21 13:56:46 +1000

    Add chess piece move and take events

--- event_types.py
+++ event_types.py
@@ -3,3 +3,7 @@ from utils.events import new_event_type
 SCENECHANGEEVENT = new_event_type()
 
 CHESSSQUARECLICKEVENT = new_event_type()
+
+CHESSPIECEMOVEEVENT = new_event_type()
+
+CHESSPIECETAKEEVENT = new_event_type()
--- game_events.py
+++ game_events.py
@@ -2,11 +2,18 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from utils.events import BaseEvent
-from event_types import SCENECHANGEEVENT, CHESSSQUARECLICKEVENT
+from event_types import (
+    SCENECHANGEEVENT,
+    CHESSSQUARECLICKEVENT,
+    CHESSPIECEMOVEEVENT,
+    CHESSPIECETAKEEVENT,
+)
 
 if TYPE_CHECKING:
     from utils.scene import Scene
     from view.board import ChessSquareView
+    from models.board import ChessSquare
+    from models.base_piece import ChessPiece
 
 
 class SceneChangeEvent(BaseEvent):
@@ -33,3 +40,51 @@ class ChessSquareClickEvent(BaseEvent):
 
     def _get_kwargs(self):
         return {"square": self.square}
+
+
+class ChessPieceMoveEvent(BaseEvent):
+    def __init__(
+        self, old_square: ChessSquare, new_square: ChessSquare, piece: ChessPiece
+    ) -> None:
+        self.old_square = old_square
+        self.new_square = new_square
+        self.piece = piece
+        super().__init__()
+
+    @property
+    def event_type(self):
+        return CHESSPIECEMOVEEVENT
+
+    def _get_kwargs(self):
+        return {
+            "old_square": self.old_square,
+            "new_square": self.new_square,
+            "piece": self.piece,
+        }
+
+
+class ChessPieceTakeEvent(BaseEvent):
+    def __init__(
+        self,
+        old_square: ChessSquare,
+        new_square: ChessPiece,
+        taking_piece: ChessPiece,
+        taken_piece: ChessPiece,
+    ) -> None:
+        self.old_square = old_square
+        self.new_square = new_square
+        self.taking_piece = taking_piece
+        self.taken_piece = taken_piece
+        super().__init__()
+
+    @property
+    def event_type(self):
+        return CHESSPIECETAKEEVENT
+
+    def _get_kwargs(self):
+        return {
+            "old_square": self.old_square,
+            "new_square": self.new_square,
+            "taking_piece": self.taking_piece,
+            "taken_piece": self.taken_piece,
+        }
