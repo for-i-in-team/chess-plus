4959be5903036df70d85ee92e875528987d2d65f
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-22 20:44:50 +1000

    Refactor Chess Board Class

--- scripts/model/board.gd
+++ scripts/model/board.gd
@@ -2,8 +2,21 @@
 
 class_name ChessBoard
 
+var size :Vector2
+var board : Array[BoardRow] = []
 
+func _init(board_size:Vector2):
+    for i in range(board_size.y):
+        size = board_size
+        board.append(BoardRow.new(i,size.x as int))
+		
 
+class BoardRow:
+    var row:Array[ChessSquare]
+    func _init(row_num:int, row_length:int, ):
+        for i in range(row_length):
+            var color : ChessBoard.SquareColor = (ChessBoard.Black.new() as ChessBoard.SquareColor) if (i+row_num)%2 == 0 else ChessBoard.White.new()
+            row.append(ChessSquare.new(color))
 
 class ChessSquare:
     var color:ChessBoard.SquareColor
--- scripts/view/chess_board.gd
+++ scripts/view/chess_board.gd
@@ -3,20 +3,13 @@ extends Node2D
 
 @export var chess_square_node : PackedScene
 @export var board_size : Vector2 = Vector2(8,8)
-var board : Array[BoardRow] = []
-
-class BoardRow:
-	var row:Array[ChessSquareView]
-	func _init(row_num:int, board_size:Vector2, chess_square_node:PackedScene, ):
-		for i in range(board_size.x):
-			var color : ChessBoard.SquareColor = (ChessBoard.Black.new() as ChessBoard.SquareColor) if (i+row_num)%2 == 0 else ChessBoard.White.new()
-			var square:ChessSquareView = chess_square_node.instantiate()
-			square.init(ChessBoard.ChessSquare.new(color),Vector2(i,row_num))
-			row.append(square)
+var board :ChessBoard
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(board_size.y):
-		board.append(BoardRow.new(i,board_size, chess_square_node))
-		for square in board[i].row:
+	board = ChessBoard.new(board_size)
+	for row in range(len(board.board)):
+		for col in range(len(board.board[row].row)):
+			var square:ChessSquareView = chess_square_node.instantiate()
+			square.init(board.board[row].row[col],Vector2(col, row))
 			add_child(square)
