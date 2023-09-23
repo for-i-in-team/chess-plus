e24f761dcd99c6741a46372add4ad0bba8970ac8
Sami Yildirim <sapphirewhalez@gmail.com>
2023-09-23 20:21:13 +1000

    Update en passant logic for pawns

--- scripts/model/traditional_pieces.gd
+++ scripts/model/traditional_pieces.gd
@@ -32,6 +32,23 @@ class Pawn:
 
 		return valid
 
+	func take(board: ChessBoard, current_square: ChessBoard.Square, target_square: ChessBoard.Square):
+		if target_square in get_valid_takes(board, current_square):
+			var taken_piece:ChessPiece = target_square.piece
+			var en_passant_squares : Array[ChessBoard.Square] = get_en_passant_pieces(target_square, board)
+			
+			current_square.piece = null
+			target_square.piece = self
+			
+			if len(en_passant_squares) > 0:
+				for square in en_passant_squares:
+					square.piece = null
+					board.events.piece_taken.emit(current_square, target_square, self, square.piece)
+			if taken_piece != null:
+				board.events.piece_taken.emit(current_square, target_square, self, target_square.piece)
+		else:
+			assert( target_square in get_valid_takes(board, current_square), "Invalid move %s -> %s" % [current_square.to_string(), target_square.to_string()])
+
 	func get_valid_takes(board:ChessBoard, current_square:ChessBoard.Square):
 		var take_squares : Array[ChessBoard.Square] = [
 			board.get_square(current_square.coordinates + color.move_direction + color.get_perpendicular_direction()),
@@ -39,7 +56,7 @@ class Pawn:
 		]
 		var valid_takes : Array[ChessBoard.Square] = []
 		for square in take_squares:
-			if square != null and ((square.piece != null and square.piece.color != color) or len(get_en_passant_pieces(square, board)) > 0):
+			if square != null and ((square.piece != null and square.piece.color != color) or  len(get_en_passant_pieces(square, board)) > 0):
 				valid_takes.append(square)
 
 
@@ -47,11 +64,13 @@ class Pawn:
 
 	func get_en_passant_pieces(square:ChessBoard.Square, board: ChessBoard) -> Array[ChessBoard.Square]:
 		var valid_en_passant_pieces : Array[ChessBoard.Square] = []
-		for row in board.squares:
+		if square.piece != null and square.piece.color == color:
+			return valid_en_passant_pieces
+		for row in board.board:
 			for ep_square in row.row:
-				if ep_square.piece != null and ep_square.piece.color != color and ep_square.piece.get("en_passantable_squares") != null:
+				if ep_square.piece != null and ep_square.piece.color != color and ep_square.piece.get("en_passantable_coords") != null:
 					var pawn : ChessPiece = ep_square.piece
-					if pawn.en_passantable_squares.contains(square):
+					if square in pawn.en_passantable_coords:
 						valid_en_passant_pieces.append(ep_square)
 		return valid_en_passant_pieces
 
@@ -68,7 +87,7 @@ static func get_traditional_board_setup():
 	
 	#Pawns
 	for i in range(8):
-		board.get_square(Vector2(i,1)).piece = Pawn.new(ChessPiece.Black.new())
-		board.get_square(Vector2(i,6)).piece = Pawn.new(ChessPiece.White.new())
+		board.get_square(Vector2(i,6)).piece = Pawn.new(ChessPiece.PieceColor.black)
+		board.get_square(Vector2(i,1)).piece = Pawn.new(ChessPiece.PieceColor.white)
 
 	return board
