1c6ff3d6e5b3530459a7214340a46b1c604166a9
Sami Yildirim <sapphirewhalez@gmail.com>
2023-11-21 19:44:16 +1100

    Create SceneManager class

---
+++ scripts/view/scene_manager.gd
@@ -0,0 +1,32 @@
+class_name SceneManager
+
+extends Node
+
+const SCENE_LOAD_DELAY : float = 0.3
+
+
+func load_scene(scene:Scene):
+	get_tree().change_scene_to_packed(scene.get_packed_scene())
+
+	while !scene.is_scene_ready(get_tree()):
+		await(get_tree().create_timer(SCENE_LOAD_DELAY).timeout)
+
+	scene.on_scene_loaded(get_tree())
+
+
+
+
+class Scene:
+	
+	func load_scene():
+		SceneManagerInstance.load_scene(self)
+
+	func get_packed_scene() -> PackedScene:
+		assert(false, "get_packed_scene is not implemented")
+		return null
+
+	func is_scene_ready(tree:SceneTree) -> bool:
+		return true
+
+	func on_scene_loaded(tree:SceneTree):
+		pass
