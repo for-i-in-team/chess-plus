e63c4058e327f7ae181aa5abb16c5f7203b44aa7
Sami Yildirim <sapphirewhalez@gmail.com>
2023-12-29 12:20:33 +1100

    Update Player List Item to include spectator option

--- scripts/view/player_list_item.gd
+++ scripts/view/player_list_item.gd
@@ -2,6 +2,8 @@ class_name PlayerListItem
 
 extends Node
 
+const SPECTATOR = 2147483647
+
 @export var color_icon : Image
 var lobby: ChessLobby
 var player: ChessLobby.ChessPlayer
@@ -14,18 +16,26 @@ func set_player(_lobby: ChessLobby, _player: ChessLobby.ChessPlayer):
 func update_display():
 	$Username.text = player.name
 	
-	$ColorSelect.clear()
+	var select = $ColorSelect
+	select.clear()
+	select.add_item("Spectator", SPECTATOR)
+	select.select(0)
 	for color in lobby.board.colors:
 		var id = lobby.board.colors.find(color)
 		var icon : Image = color_icon.duplicate()
 		icon.fill(color.color)
 		icon.resize(32,32)
-		$ColorSelect.add_icon_item(ImageTexture.create_from_image(icon), color.name, id)
+		select.add_icon_item(ImageTexture.create_from_image(icon), color.name, id)
 		if color == player.color:
-			$ColorSelect.select(id)
+			select.select(select.get_item_index(id))
 
 func leave_game():
 	lobby.kick(player.id)
 
-func _on_ColorSelect_item_selected(id):
-	lobby.update_player_color(player.id, lobby.board.colors[id])
+func _on_ColorSelect_item_selected(index):
+	var id = $ColorSelect.get_item_id(index)
+	if id == SPECTATOR:
+		lobby.update_player_color(player.id, null)
+	else:
+		lobby.update_player_color(player.id, lobby.board.colors[id])
+
